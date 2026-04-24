0922c535963d513eec0ffcceb2cc118618b6065f
Muhammet Akduman <akdumanmxc@gmail.com>
2026-04-24 23:37:41 +0300

    JwtServiceMethod Already , (TokenExpired and New Toekn Created)

--- src/main/java/com/muhammetakduman/refresh_token_auth/security/JwtService.java
+++ src/main/java/com/muhammetakduman/refresh_token_auth/security/JwtService.java
@@ -1,4 +1,58 @@
 package com.muhammetakduman.refresh_token_auth.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+
+@Service
 public class JwtService {
+    @Value("${jwt.secret}")
+    private String secretKey;
+    @Value("${jwt.access-token-expiration-ms}")
+    private Long accessTokenExpirationMs;
+
+    public String genereteAccessToken(String email){
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + accessTokenExpirationMs);
+
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(now)
+                .expiration(expiryDate)
+                .signWith(getSigningKey())
+                .compact();
+    }
+    public String extractEmail(String token){
+        return extractAllClaims(token).getSubject();
+
+    }
+    public boolean isTokenValid(String token, String email){
+        String tokenEmail = extractEmail(token);
+        return (tokenEmail.equals(email) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        Date expiration = extractAllClaims(token).getExpiration();
+        return expiration.before(new Date());
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = secretKey.getBytes();
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 }
