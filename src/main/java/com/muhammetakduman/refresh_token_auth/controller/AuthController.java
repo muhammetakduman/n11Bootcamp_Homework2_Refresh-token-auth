807f2f553c30af087fdb3659d1c01a59c5c034d1
Muhammet Akduman <akdumanmxc@gmail.com>
2026-04-24 23:57:44 +0300

    app.propertis update, Controller write 2 Post Method (Login , Register)

--- src/main/java/com/muhammetakduman/refresh_token_auth/controller/AuthController.java
+++ src/main/java/com/muhammetakduman/refresh_token_auth/controller/AuthController.java
@@ -1,4 +1,29 @@
 package com.muhammetakduman.refresh_token_auth.controller;
 
+
+import com.muhammetakduman.refresh_token_auth.dto.request.LoginRequest;
+import com.muhammetakduman.refresh_token_auth.dto.request.RegisterRequest;
+import com.muhammetakduman.refresh_token_auth.dto.response.AuthResponse;
+import com.muhammetakduman.refresh_token_auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public void register(@RequestBody RegisterRequest request){
+        authService.register(request);
+    }
+
+    @PostMapping
+    public AuthResponse login(@RequestBody LoginRequest request){
+        return authService.login(request);
+    }
 }
