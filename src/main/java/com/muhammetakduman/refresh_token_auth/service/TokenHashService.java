efa2deda38edf164b61a5d35548a96f2b4031752
Muhammet Akduman <akdumanmxc@gmail.com>
2026-04-24 23:22:47 +0300

    Refresh token mask in db ( SHA 256)

--- src/main/java/com/muhammetakduman/refresh_token_auth/service/TokenHashService.java
+++ src/main/java/com/muhammetakduman/refresh_token_auth/service/TokenHashService.java
@@ -1,4 +1,28 @@
 package com.muhammetakduman.refresh_token_auth.service;
 
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
+@Service
 public class TokenHashService {
+    public String hashToken(String token){
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] encodeHash = digest.digest(token.getBytes(StandardCharsets.UTF_8));
+            StringBuilder hexString = new StringBuilder();
+            for(byte b : encodeHash){
+                String hex = Integer.toHexString(0xff & b);
+                if(hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+            return hexString.toString();
+
+        } catch (Exception e) {
+            throw new RuntimeException("Token hash could not be genereted", e);
+        }
+    }
 }
