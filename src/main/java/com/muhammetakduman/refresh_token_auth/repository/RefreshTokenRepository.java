f2637b8e70fd9c31a5abc3b1aedcbed93235f8e2
Muhammet Akduman <akdumanmxc@gmail.com>
2026-04-24 22:56:04 +0300

    Find Db refreshtoken Hash

--- src/main/java/com/muhammetakduman/refresh_token_auth/repository/RefreshTokenRepository.java
+++ src/main/java/com/muhammetakduman/refresh_token_auth/repository/RefreshTokenRepository.java
@@ -1,4 +1,14 @@
 package com.muhammetakduman.refresh_token_auth.repository;
 
-public class RefreshTokenRepository {
+import com.muhammetakduman.refresh_token_auth.entitiy.RefreshToken;
+import com.muhammetakduman.refresh_token_auth.entitiy.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken,Long> {
+    //refresh token kullanıcıdan alıp db katmanında karşılaştırmak için token hash'ini kullanacağım
+    Optional<RefreshToken> findByTokenHash(String tokenHash);
+    void deleteByUser(User user);
+    void deleteByTokenHash(String tokenHash);
 }
--- src/main/java/com/muhammetakduman/refresh_token_auth/repository/UserRepository.java
+++ src/main/java/com/muhammetakduman/refresh_token_auth/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.muhammetakduman.refresh_token_auth.repository;
 
-public class UserRepository {
+import com.muhammetakduman.refresh_token_auth.entitiy.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+    boolean exxistsByEmail(String email);
 }
