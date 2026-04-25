ac82448a8e427080fb38a474fe57d1cbe3819e40
Muhammet Akduman <akdumanmxc@gmail.com>
2026-04-25 19:40:11 +0300

    cooki congig

--- src/main/java/com/muhammetakduman/refresh_token_auth/config/CookieConfig.java
+++ src/main/java/com/muhammetakduman/refresh_token_auth/config/CookieConfig.java
@@ -1,4 +1,24 @@
 package com.muhammetakduman.refresh_token_auth.config;
 
+import jakarta.servlet.http.Cookie;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CookieConfig {
+    public Cookie createRefreshTokenCookie(String refreshToken){
+        Cookie cookie = new Cookie("refreshToken", refreshToken);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(false);
+        cookie.setPath("/");
+        cookie.setMaxAge(5 * 60);
+        return cookie;
+    }
+    public Cookie clearRefreshTokenCookie(){
+        Cookie cookie = new Cookie("refreshToken", null);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(false);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+        return cookie;
+    }
 }
